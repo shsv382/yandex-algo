7a3b9e80cea5e1c636a8931eac509802b9d63c2c
Sergey Shabanov <shsv382@gmail.com>
2023-02-18 21:26:09 +0300

    Added task 20

--- app.js
+++ app.js
@@ -1,64 +1,33 @@
-class Queue {
-    constructor() {
-        this.array = new Array(10);
-        this.head = 0;
-        this.tail = 1;
-        this.length = 0;
-    }
-    push(n) {
-        this.array[this.tail] = parseInt(n);
-        this.tail = (this.tail + 1) % 10;
-        this.length++;
-    }
-    pop() {
-        if (this.length < 1) return 'error';
-        this.head = (this.head + 1) % 10;
-        let answer = this.array[this.head];
-        this.array[this.head] = null;
-        this.length--;
-        return answer;
-    }
-    front() {
-        if (this.length < 1) return 'error';
-        return this.array[(this.head + 1)%10];
-    }
-    size() {
-        return this.length;
-    }
-}
+var readline = require('readline');
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
-function main(args) {
-    let player1 = new Queue();
-    let player2 = new Queue();
-    
-    args = args.map(item => item.split(' '));
-    for(let i=0; i<5; i++) {
-        player1.push(args[0][i]);
-        player2.push(args[1][i]);
-    }
-    
-    let count = 0;
-    let a, b;
-    while (player1.size() > 0 && player2.size() > 0 && count < 10**6) {
-        [a, b] = [player1.pop(), player2.pop()];
-        if((a > b && !(a === 9 && b === 0)) || (a === 0 && b === 9)) {
-            player1.push(a);
-            player1.push(b);
-        } else {
-            player2.push(a);
-            player2.push(b);
-        }
-        count++;
-    }
-    if (!player1.size()) {
-        console.log(`second ${count}`)
-    } else if (!player2.size()) {
-        console.log(`first ${count}`)
+
+let counter = 0;
+
+rl.on('line', function (cmd) {
+    if (counter === 0) {
+        counter++;
     } else {
-        console.log('botva')
+        cmd = cmd.split(' ');
+        for (let i = Math.floor(cmd.length / 2 - 1); i >= 0; i--) {
+            let pos = i;
+            while(pos * 2 + 2 < cmd.length) {
+                let min_son_idx = pos * 2 + 1;
+                if (cmd[pos * 2 + 2] < cmd[min_son_idx]) {
+                    min_son_idx = pos * 2 + 2;
+                }
+                if (cmd[pos] > cmd[min_son_idx]) {
+                    [cmd[pos], cmd[min_son_idx]] = [cmd[min_son_idx], cmd[pos]];
+                    pos = min_son_idx;
+                } else {
+                    break;
+                }
+            }
+        }
     }
-    return
-}
+    console.log(cmd);
+});
 
-
-console.log(main([ '2 4 6 8 0', '1 3 5 7 9' ]));
